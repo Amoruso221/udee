a274bf5bd542bc3425d23a737db342caeea1a684
Matias Amoruso <matias.amoruso@gmail.com>
2021-05-12 18:52:38 -0300

    first commit

---
+++ src/main/java/edu/utn/udee/Udee/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.utn.udee.Udee;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UdeeApplication.class);
+	}
+
+}
---
+++ src/test/java/edu/utn/udee/Udee/UdeeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.udee.Udee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d90e1392a9fac4df3118e21539edbd1e22d4e262
Matias Amoruso <matias.amoruso@gmail.com>
2021-05-14 17:36:56 -0300

    add Address and client domain

---
+++ src/main/java/edu/utn/udee/Udee/domain/Client.java
@@ -0,0 +1,35 @@
+package edu.utn.udee.Udee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    Integer id;
+
+    @Column
+    String name;
+
+    @Column
+    String surname;
+
+    @Column(unique=true)
+    Integer dni;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "client")
+    List<Address> address;
+
+}

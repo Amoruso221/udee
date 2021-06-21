586d260f2914c6c8d381bd42b67b116c88e9515d
Matias Amoruso <matias.amoruso@gmail.com>
2021-06-21 12:16:32 -0300

    some client controller test

---
+++ src/test/java/edu/utn/udee/Udee/TestUtils/ClientTestUtils.java
@@ -0,0 +1,35 @@
+package edu.utn.udee.Udee.TestUtils;
+
+import edu.utn.udee.Udee.domain.Client;
+import edu.utn.udee.Udee.dto.ClientDto;
+
+public class ClientTestUtils {
+
+    public static ClientDto getClientDto() {
+        return ClientDto.builder()
+                .name("Matias")
+                .surname("Amoruso")
+                .dni(1234)
+                .address(null)
+                .build();
+    }
+
+    public static Client getClientWithoutId() {
+        return Client.builder()
+                .name("Matias")
+                .surname("Amoruso")
+                .dni(1234)
+                .address(null)
+                .build();
+    }
+
+    public static Client getClientWithId(){
+        return Client.builder()
+                .id(1)
+                .name("Matias")
+                .surname("Amoruso")
+                .dni(1234)
+                .address(null)
+                .build();
+    }
+}
